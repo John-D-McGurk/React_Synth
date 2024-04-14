e8427bf988c5324bc813d45e169fcf6af95fab1b
John-D-McGurk <john.d.mcgurk@hotmail.com>
2024-04-14 08:50:02 +0200

    implemented basic panel class with dynamic input components

--- components/Panel.jsx
+++ components/Panel.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import Knob from "./Knob";
+
+const components = {
+  Knob: Knob,
+};
 
 export default function Panel(props) {
-  let knobs;
-  return (
-    <div className="panel">
-      {props.label && <span className="knob--label">{props.label}</span>}
-    </div>
-  );
+  let inputs = [];
+  let i = 0;
+  console.log(props.names.keys);
+  for (const [key, value] of Object.entries(props.names)) {
+    const Type = components[value.type];
+    inputs.push(<Type label={Object.keys(props.names)[i]} />);
+    i++;
+  }
+  return <div className="panel">{inputs}</div>;
 }
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import Knob from "../components/Knob";
+import Panel from "../components/Panel";
+import basic from "../synthesizers/basic.json";
+
+const components = {
+  Knob: Knob,
+};
 
 export default function App() {
+  const Type = components[basic.panels.misc.inputs.gain.type];
   return (
     <>
-      <Knob label="test" value="0" />
+      <Panel
+        names={basic.panels.misc.inputs}
+        columns={basic.panels.misc.columns}
+      />
     </>
   );
 }
