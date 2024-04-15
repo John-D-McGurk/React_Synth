c3a5edf92a730453ef9bdb6b9ee76f3472218da4
John-D-McGurk <john.d.mcgurk@hotmail.com>
2024-04-15 19:42:15 +0200

    created osc object which fixes one of "sticky keys" bugs

--- synthesizers/basic/basic.js
+++ synthesizers/basic/basic.js
@@ -1,3 +1,5 @@
+// import { env } from "../../.eslintrc.cjs";
+
 const ctx = new AudioContext();
 const oscList = [];
 for (let i = 0; i < 9; i++) {
@@ -11,55 +13,71 @@ let customWaveform = null;
 let sineTerms = null;
 let cosineTerms = null;
 
+class Osc {
+  constructor(ctx, type, freq, envelope, out) {
+    this.ctx = ctx;
+    this.envelope = envelope;
+    this.osc = ctx.createOscillator();
+    this.osc.frequency.value = freq;
+    this.osc.type = type || "sine";
+    this.envGain = ctx.createGain();
+    this.envGain.gain.value = 0;
+    this.osc.connect(this.envGain);
+    this.envGain.connect(out);
+    this.easing = 0.005;
+    this.osc.start();
+    this.start();
+  }
+  start() {
+    let now = this.ctx.currentTime;
+    this.envGain.gain.cancelScheduledValues(now);
+    this.envGain.gain.setValueAtTime(0, now + this.easing);
+    this.envGain.gain.linearRampToValueAtTime(
+      1,
+      now + this.envelope.attack + this.easing
+    );
+    this.envGain.gain.linearRampToValueAtTime(
+      this.envelope.sustain,
+      now + this.envelope.attack + this.envelope.decay + this.easing
+    );
+  }
+  stop() {
+    let now = this.ctx.currentTime;
+    let currentGain = this.envGain.gain.value;
+    this.envGain.gain.cancelScheduledValues(now);
+    this.envGain.gain.setValueAtTime(currentGain, now);
+
+    this.envGain.gain.setTargetAtTime(
+      0,
+      now,
+      this.envelope.release + this.easing
+    );
+    setTimeout(() => {
+      this.osc.disconnect();
+    }, 10000);
+  }
+}
+
 export function audioSetup(state) {
   mainGainNode.gain.value = state.panels.controls.gain / 100;
 }
 
-export function playTone(freq, state) {
-  ctx.resume();
-  const now = ctx.currentTime;
-
-  const osc = ctx.createOscillator(),
-    envelope = ctx.createGain(),
-    attack = state.panels.envelope.attack,
-    decay = state.panels.envelope.decay,
-    sustain = state.panels.envelope.sustain;
-
-  osc.connect(envelope);
-  envelope.connect(mainGainNode);
-
-  envelope.gain.setValueAtTime(0, now);
-  envelope.gain.linearRampToValueAtTime(1, now + attack);
-  envelope.gain.linearRampToValueAtTime(sustain, now + attack + decay);
-  osc.frequency.value = freq;
-  osc.type = "sine";
-
-  osc.start();
-
-  const oscObject = { osc: osc, env: envelope };
-
-  return oscObject;
-}
-
 export function addOsc(dataset, state) {
-  const freq = state.keys[dataset.note][dataset.octave].freq;
-  oscList[dataset.octave][dataset.note] = playTone(freq, state);
+  const freq = state.keys[dataset.note][dataset.octave].freq,
+    envelope = state.panels.envelope;
+  oscList[dataset.octave][dataset.note] = new Osc(
+    ctx,
+    "sine",
+    freq,
+    envelope,
+    mainGainNode
+  );
 }
 
 export function removeOsc(dataset, state) {
   const octave = dataset.octave,
     note = dataset.note;
-
-  if (oscList[octave] && oscList[octave][note]) {
-    const osc = oscList[octave][note].osc,
-      env = oscList[octave][note].env,
-      now = ctx.currentTime,
-      release = state.panels.envelope.release;
-
-    env.gain.linearRampToValueAtTime(0, now + release);
-    setTimeout(() => {
-      osc.stop();
-      delete oscList[octave][note];
-    }, release * 1000);
+  if (oscList[octave][note]) {
+    oscList[octave][note].stop();
   }
 }
