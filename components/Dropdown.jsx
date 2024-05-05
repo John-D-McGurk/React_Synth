b0e508aadbb84353b36b9386fe2f646c68302edf
John-D-McGurk <john.d.mcgurk@hotmail.com>
2024-05-05 14:38:03 +0200

    added to lowercase on ket inputs

--- components/Dropdown.jsx
+++ components/Dropdown.jsx
@@ -3,19 +3,21 @@ import React from "react";
 export default function Button(props) {
   const value = props.settings.panels[props.panel][props.label];
   const options = props.contents.map((option) => {
-    console.log(value);
-    console.log(option);
-    return <option selected={option === value} value={option}>{option}</option>;
+    return (
+      <option selected={option === value} value={option}>
+        {option}
+      </option>
+    );
   });
-  console.log(value)
   return (
     <div className="dropdown">
       <span className="dropdown--label">{props.label}</span>
-      <select 
-       data-panel={props.panel}
+      <select
+        data-panel={props.panel}
         className={props.label}
         name={props.label}
-        onChange={props.handleChange}>
+        onChange={props.handleChange}
+      >
         {options}
       </select>
     </div>
--- components/Keyboard.jsx
+++ components/Keyboard.jsx
@@ -6,18 +6,18 @@ export default function Keyboard(props) {
   const notes = ["C", "D", "E", "F", "G", "A", "B"],
     startOctave = props.octaves[0],
     noteIndex = {
-      "C": 0,
+      C: 0,
       "C#": 1,
-      "D": 2,
+      D: 2,
       "D#": 3,
-      "E": 4,
-      "F": 5,
+      E: 4,
+      F: 5,
       "F#": 6,
-      "G": 7,
+      G: 7,
       "G#": 8,
-      "A": 9,
+      A: 9,
       "A#": 10,
-      "B": 11,
+      B: 11,
     },
     keyMap = {
       a: { note: `C`, octave: startOctave },
@@ -39,31 +39,28 @@ export default function Keyboard(props) {
     },
     state = props.state,
     keyRefs = useRef([]);
-    console.log(state)
 
   useEffect(() => {
     function getKeyDOM(e) {
-
-      const keyPressed = keyMap[e.key],
+      const keyPressed = keyMap[e.key.toLowerCase()],
         keyIdx = noteIndex[keyPressed.note],
         fullIdx = keyIdx + keyPressed.octave * 12,
         pressedKey = keyRefs.current[fullIdx];
       return pressedKey;
-
     }
     function handleKeyDown(e) {
-      if (e.key in keyMap) {
+      if (e.key.toLowerCase() in keyMap) {
         const pressedKey = getKeyDOM(e);
         if (!pressedKey.dataset.pressed) {
-          props.notePressed(pressedKey, state, false)
+          props.notePressed(pressedKey, state, false);
 
-          e.target.addEventListener('keyup', handleKeyUp);
+          e.target.addEventListener("keyup", handleKeyUp);
         }
       }
     }
-// Bug: if another key that isn't mapped is pressed a time of release function won't fire
+    // Bug: if another key that isn't mapped is pressed a time of release function won't fire
     function handleKeyUp(e) {
-      if (e.key in keyMap) {
+      if (e.key.toLowerCase() in keyMap) {
         const pressedKey = getKeyDOM(e);
         if (pressedKey.dataset.pressed) {
           props.noteReleased(pressedKey, false);
@@ -71,25 +68,24 @@ export default function Keyboard(props) {
       }
     }
 
-    document.addEventListener('keydown', handleKeyDown);
+    document.addEventListener("keydown", handleKeyDown);
 
     return function cleanup() {
-      document.removeEventListener('keydown', handleKeyDown);
-      document.removeEventListener('keyup', handleKeyUp);
-    }
+      document.removeEventListener("keydown", handleKeyDown);
+      document.removeEventListener("keyup", handleKeyUp);
+    };
+  }, []);
 
-  }, [])
+  const octaveList = [0, 1, 2, 3, 4, 5, 6, 7];
 
-  const octaveList = [0, 1, 2, 3, 4, 5, 6, 7]
-
-  octaveList.forEach(octave => {
+  octaveList.forEach((octave) => {
     keyboard = keyboard.concat(
       notes.map((note) => {
         const sharpNote = note + "#",
           noteIdx = 12 * octave /*(octave - startOctave)*/ + noteIndex[note];
         return (
           <div
-            ref={el => keyRefs.current[noteIdx] = el}
+            ref={(el) => (keyRefs.current[noteIdx] = el)}
             data-note={note}
             data-octave={octave}
             className="keyboard--white-note"
@@ -97,12 +93,12 @@ export default function Keyboard(props) {
             onMouseUp={(e) => props.noteReleased(e, state, true)}
             onMouseOver={(e) => props.notePressed(e, state, true)}
             onMouseLeave={(e) => props.noteReleased(e, state, true)}
-          // data-pressed={props.state.keys[note][octave].active}
+            // data-pressed={props.state.keys[note][octave].active}
           >
             <span className="keyboard--white-note-label">{note + octave} </span>
             {note !== "E" && note != "B" && (
               <div
-                ref={el => keyRefs.current[noteIdx + 1] = el}
+                ref={(el) => (keyRefs.current[noteIdx + 1] = el)}
                 data-note={note + "#"}
                 data-octave={octave}
                 className="keyboard--black-note"
@@ -110,7 +106,7 @@ export default function Keyboard(props) {
                 onMouseUp={(e) => props.noteReleased(e, state, true)}
                 onMouseOver={(e) => props.notePressed(e, state, true)}
                 onMouseLeave={(e) => props.noteReleased(e, state, true)}
-              // data-pressed={props.state.keys[sharpNote][octave].active}
+                // data-pressed={props.state.keys[sharpNote][octave].active}
               >
                 <span className="keyboard--black-note-label">{sharpNote}</span>
               </div>
@@ -119,6 +115,6 @@ export default function Keyboard(props) {
         );
       })
     );
-  })
+  });
   return <div id="keyboard">{keyboard}</div>;
 }
