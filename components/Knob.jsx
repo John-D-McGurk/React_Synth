f260ea58db980bc2e4192e81372c95d807c5d0bf
John-D-McGurk <john.d.mcgurk@hotmail.com>
2024-05-09 13:44:16 +0200

    styled knobs

--- components/Knob.jsx
+++ components/Knob.jsx
@@ -2,8 +2,10 @@ import React, { useState } from "react";
 
 export default function Knob(props) {
   const value = props.settings[props.panel][props.label];
+
   return (
     <div className="knob">
+      <div className="knob--visual"></div>
       <span className="knob--label">{props.label}</span>
       <input
         data-panel={props.panel}
@@ -14,7 +16,12 @@ export default function Knob(props) {
         step={props.step}
         value={value}
         onChange={props.handleChange}
-        className="slider"
+        className="knob--slider"
+        style={{
+          "--min": props.min || 0,
+          "--max": props.max || 100,
+          "--value": value || 0,
+        }}
       />
       <span>{value}</span>
     </div>
